a4ebcd3027647f7649fb3889b01cfbc456661704
박상우 <sorlti6952@gmail.com>
2024-02-07 16:44:09 +0900

    DESIGN:POST - 포스트 이미지 transition으로 변경 - post id -> class로 변경 (컴포넌트화 예정)

--- prob22/src/post.js
+++ prob22/src/post.js
@@ -1,222 +1,65 @@
-let prev = document.getElementById('leftArrow')
-let next = document.getElementById('rightArrow')
-
-let prev2 = document.getElementById('leftArrow2')
-let next2 = document.getElementById('rightArrow2')
-
-let prev3 = document.getElementById('leftArrow3')
-let next3 = document.getElementById('rightArrow3')
+let prev = document.querySelector('.leftArrow')
+let next = document.querySelector('.rightArrow')
 
 
-let imgs = document.querySelectorAll('#sectionImage ul li')
-let imgs2 = document.querySelectorAll('#sectionImage2 ul li')
-let imgs3 = document.querySelectorAll('#sectionImage3 ul li')
+let imgs = document.querySelectorAll('.sectionImage ul li')
+let bullets = document.querySelectorAll('.sectionImageBullets label')
 
-let bullets = document.querySelectorAll('#sectionImageBullets label')
-let bullets2 = document.querySelectorAll('#sectionImageBullets2 label')
-let bullets3 = document.querySelectorAll('#sectionImageBullets3 label')
+let index = 0
+let move = 0
 
-window.onload = function() {
-    showImg(0)
-    showImg2(0)
-    showImg3(0)
-    showBullet(0)
-    showBullet2(0)
-    showBullet3(0)
-    showArrow()
+window.onload = function () {
+    changeBullet()
 }
 
-let imgNum = 0
-let imgNum2 = 0
-let imgNum3 = 0
+prev.addEventListener('click', function () {
+    if (index != 0) {
+        index--
+    }
+    changeImage(-1)
+    changeBullet()
+})
 
-function showImg(idx) {
-    imgs.forEach(function(img, index) {
-        if (index == idx) {
-            img.style.display = 'block'
-        } else {
-            img.style.display = 'none'
-        }
-    })
+next.addEventListener('click', function () {
+    if (index != imgs.length - 1) {
+        index++
+    }
+    changeImage(1)
+    changeBullet()
+})
+
+function changeImage(num) {
+    // -1이면 왼쪽, 1이면 오른쪽
+
+    if (num == -1 && move < 0) {
+        move += 500
+    } else if (num == 1 && move > -500 * (imgs.length - 1)) {
+        move -= 500
+    }
+    document.querySelector('.sectionImage ul li').style.marginLeft = `${move}px`
+    document.querySelector('.sectionImage ul li').style.transition = '0.5s'
 }
-function showImg2(idx) {
-    imgs2.forEach(function(img, index) {
-        if (index == idx) {
-            img.style.display = 'block'
+
+function changeBullet() { 
+    bullets.forEach((bullet, i) => {
+        //현재 이미지는 흰색, 그 외 회색
+        if (i == index) {
+            bullet.style.backgroundColor = 'rgba(255,255,255,0.7)'
         } else {
-            img.style.display = 'none'
-        }
-    })
-}
-function showImg3(idx) {
-    imgs3.forEach(function(img, index) {
-        if (index == idx) {
-            img.style.display = 'block'
-        } else {
-            img.style.display = 'none'
+            bullet.style.backgroundColor = 'rgba(0,0,0,0.3)'
         }
     })
 }
 
-function showBullet(idx) {
-    bullets.forEach(function(b, index) {
-        if (index == idx) {
-            b.style.backgroundColor = 'rgba(255,255,255,0.8)'
-        } else {
-            b.style.backgroundColor = 'rgba(0,0,0,0.5)'
-        }
-    })
-}
-
-function showBullet2(idx) {
-    bullets2.forEach(function(b, index) {
-        if (index == idx) {
-            b.style.backgroundColor = 'rgba(255,255,255,0.8)'
-        } else {
-            b.style.backgroundColor = 'rgba(0,0,0,0.5)'
-        }
-    })
-}
-
-function showBullet3(idx) {
-    bullets3.forEach(function(b, index) {
-        if (index == idx) {
-            b.style.backgroundColor = 'rgba(255,255,255,0.8)'
-        } else {
-            b.style.backgroundColor = 'rgba(0,0,0,0.5)'
-        }
-    })
-}
-
-function showArrow() {
-    if (imgNum == 0) {
-        //right arrow style none
-        prev.style.visibility = 'hidden'
-        next.style.visibility = 'visible'
-    } else if (imgNum == imgs.length - 1) {
-        prev.style.visibility = 'visible'
-        next.style.visibility = 'hidden'
-    } else {
-        prev.style.visibility = 'visible'
-        next.style.visibility = 'visible'
-    }
-
-    if (imgNum2 == 0) {
-        //right arrow style none
-        prev2.style.visibility = 'hidden'
-        next2.style.visibility = 'visible'
-    } else if (imgNum2== imgs2.length - 1) {
-        prev2.style.visibility = 'visible'
-        next2.style.visibility = 'hidden'
-    } else {
-        prev2.style.visibility = 'visible'
-        next2.style.visibility = 'visible'
-    }
-
-    if (imgNum3 == 0) {
-        //right arrow style none
-        prev3.style.visibility = 'hidden'
-        next3.style.visibility = 'visible'
-    } else if (imgNum3 == imgs3.length - 1) {
-        prev3.style.visibility = 'visible'
-        next3.style.visibility = 'hidden'
-    } else {
-        prev3.style.visibility = 'visible'
-        next3.style.visibility = 'visible'
-    }
-}
-
-prev.onclick = function () {
-    imgNum--
-    if (imgNum < 0) {
-        alert('불가능')
-        imgNum = 0
-    }
-    showBullet(imgNum)
-    showArrow()
-    showImg(imgNum)
-}
-
-next.onclick = function () {
-    imgNum++
-    if (imgNum > 2) {
-        alert('불가능')
-        imgNum = 2
-    }
-    showBullet(imgNum)
-    showArrow()
-    showImg(imgNum)
-}
-
-prev2.onclick = function () {
-    imgNum2--
-    if (imgNum2 < 0) {
-        alert('불가능')
-        imgNum2 = 0
-    }
-    showBullet2(imgNum2)
-    showArrow()
-    showImg2(imgNum2)
-}
-
-next2.onclick = function () {
-    imgNum2++
-    if (imgNum2 > 2) {
-        alert('불가능')
-        imgNum2 = 2
-    }
-    showBullet2(imgNum2)
-    showArrow()
-    showImg2(imgNum2)
-}
-
-prev3.onclick = function () {
-    imgNum3--
-    if (imgNum3 < 0) {
-        alert('불가능')
-        imgNum3 = 0
-    }
-    showBullet3(imgNum3)
-    showArrow()
-    showImg3(imgNum3)
-}
-
-next3.onclick = function () {
-    imgNum3++
-    if (imgNum3 > 2) {
-        alert('불가능')
-        imgNum3 = 2
-    }
-    showBullet3(imgNum3)
-    showArrow()
-    showImg3(imgNum3)
-}
 
 // -------------------------footer-----------------------
 function toggleLike() {
     let like = document.getElementById('sectionFooterImgLike')
-    let like2 = document.getElementById('sectionFooterImgLike2')
-    let like3 = document.getElementById('sectionFooterImgLike3')
-    
     let imgSrc = like.src
-    let imgSrc2 = like.src
-    let imgSrc3 = like.src
 
-    if(imgSrc.includes('fill')) {
+    if (imgSrc.includes('fill')) {
         like.src = './asset/like.png'
     } else {
         like.src = './asset/like_fill.png'
-    }
-
-    if(imgSrc2.includes('fill')) {
-        like2.src = './asset/like.png'
-    } else {
-        like2.src = './asset/like_fill.png'
-    }
-
-    if(imgSrc3.includes('fill')) {
-        like3.src = './asset/like.png'
-    } else {
-        like3.src = './asset/like_fill.png'
     }
 }
